d120769a3d491a13c851d06da6e807e5cf396246
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-01 10:57:39 +0800

    Add completed JS Stack Kata done in TDD compliant way

---
+++ src/stack.js
@@ -0,0 +1,18 @@
+module.exports = class Stack {
+  constructor() {
+    this.array = [];
+  }
+
+  push(item) {
+    this.array.push(item);
+    return this.array;
+  }
+
+  pop() {
+    return this.array.pop();
+  }
+
+  peek() {
+    return this.array[0];
+  }
+}
---
+++ test/stack.test.js
@@ -0,0 +1,57 @@
+const Stack = require("../src/stack");
+
+let stack;
+beforeEach(() => {
+  stack = new Stack()
+});
+
+describe('stack', () => {
+  it('should create stack object with an array', () => {
+    expect(stack.array).toEqual([]);
+  })
+
+  describe('push', () => {
+    it('should push item into array', () => {
+      expect(stack.push("Bob")).toEqual(["Bob"]);
+    });
+
+    it('should push items in an order into array', () => {
+      expect(stack.push("Bob")).toEqual(["Bob"]);
+      expect(stack.push("Sarah")).toEqual(["Bob", "Sarah"]);
+    });
+
+    it('should push, pop and push items in an order into array', () => {
+      stack.push("Bob");
+      stack.push("Sarah");
+      stack.pop();
+      expect(stack.push("Paul")).toEqual(["Bob", "Paul"]);
+    });
+  });
+
+  describe('pop', () => {
+    it('should pop item from array', () => {
+      stack.push("Bob");
+      stack.push("Sarah");
+      expect(stack.pop()).toEqual("Sarah");
+    });
+
+    it('should pop items in an order from array', () => {
+      stack.push("Bob");
+      stack.push("Sarah");
+      stack.pop()
+      expect(stack.pop()).toEqual("Bob");
+    });
+  });
+
+  describe('peek', () => {
+    it('should return undefined if the array is empty', () => {
+      expect(stack.peek()).toBeUndefined();
+    });
+
+    it('should return the first element of stack without mutating array', () => {
+      stack.push("Bob");
+      expect(stack.peek()).toEqual("Bob");
+      expect(stack.array).toEqual(["Bob"]);
+    });
+  });
+});
